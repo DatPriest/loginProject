45ec7bd426c6454701e9932b8addd8253bf2faab
AboushH <haneef.aboush@3txpert.com>
2022-01-30 22:07:36 +0100

    registerWithlogin

--- frontend/src/app/component/login-view/login-view.component.ts
+++ frontend/src/app/component/login-view/login-view.component.ts
@@ -3,6 +3,9 @@ import {Users} from "../../model/Users";
 import {LoginDataService} from "../../service/login-data.service";
 import {Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
+import {FormBuilder, FormGroup, NgForm} from "@angular/forms";
+import {AuthenticationService} from "../../service/authentication/authentication.service";
+
 
 @Component({
   selector: 'app-login-view',
@@ -12,21 +15,37 @@ import {HttpClient} from "@angular/common/http";
 export class LoginViewComponent implements OnInit {
 
   User : User;
+  form : FormGroup;
+  signupForm!:FormGroup
 
-  constructor(private loginDataService: LoginDataService, private router : Router, private http: HttpClient) {
+  constructor(private authenticationService: AuthenticationService, private loginDataService: LoginDataService, private router : Router, private http: HttpClient,private formBuilder: FormBuilder) {
 
-    this.User = new Users(null,null,null);
+    this.form = this.formBuilder.group({
+      name: '',
+      password: ''
+
+    });
+    //this.User = new Users(null, null, null);
 
   }
 
   ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      name:[''],
+      password:['']})
   }
-  save() {
+  onSubmit(signInForm: NgForm){
+    console.log(signInForm.value);
+    const signInData = new Users(signInForm.value.email, signInForm.value.password);
+    this.authenticationService.authenticate(signInData);
+  }
+  /*save() {
     this.loginDataService.registerUser(this.User);
-  }
+  }*/
+  /*
   register() : void {
     this.http.post('http://localhost:8000/api/register',{}).subscribe(() =>this.router.navigate(['/register']));
-  }
+  }*/
   //change(event) {alert(event.target.value);}
 
 
--- frontend/src/app/service/login-data.service.ts
+++ frontend/src/app/service/login-data.service.ts
@@ -7,12 +7,16 @@ import { User } from '../model/User';
 export class LoginDataService {
   Users: User[] = [];
   constructor() {
+<<<<<<< HEAD
     this.Users.push(new User(1,'haneef','123'));
 
   }
   registerUser(User : User){
     this.Users.push(User);
 
+=======
+    //this.Users.push(new Users(1,'haneef','123'));
+>>>>>>> a278e9f (registerWithlogin)
 
   }
 
