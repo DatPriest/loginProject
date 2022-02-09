386dabf03795b1c02ac78b6ce5f0d2ab737bbeb2
DatPriest <43251311+DatPriest@users.noreply.github.com>
2022-02-09 10:07:57 +0100

    added some beauty additions

--- frontend/src/app/component/employee-creation-view/employee-creation-view.component.ts
+++ frontend/src/app/component/employee-creation-view/employee-creation-view.component.ts
@@ -10,29 +10,39 @@ import {Employee} from "../../model/Employee";
   styleUrls: ['./employee-creation-view.component.css']
 })
 export class EmployeeCreationViewComponent implements OnInit {
-  lastname:string;
-  firstname:string;
-  street:string;
-  postcode:string;
-  city:string;
-  phone:string;
-  constructor(private authentifcationservice: AuthenticationService, private router: Router, private employeeservice:EmployeeService) { }
+  lastname: string;
+  firstname: string;
+  street: string;
+  postcode: string;
+  city: string;
+  phone: string;
+  constructor(
+    private authentifcationService: AuthenticationService,
+    private router: Router,
+    private employeeService: EmployeeService) { }
 
-  isAuth(): boolean{
-    return this.authentifcationservice.isAuthenticated;
+  isAuth(): boolean {
+    return this.authentifcationService.isAuthenticated;
   }
 
-  logout(){
-    this.authentifcationservice.logout();
+  logout() {
+    this.authentifcationService.logout();
   }
-  addEmployee(){
-    this.employeeservice.postEmployees(new Employee(0,this.lastname,this.firstname,this.street,this.postcode,this.city,this.phone));
-    setTimeout(()=>{
-      this.employeeservice.getEmployees();
-      this.router.navigate(['employee']);
-    }, 1000);
+  addEmployee() {
+    this.employeeService.postEmployees(
+      new Employee(0,
+        this.lastname,
+        this.firstname,
+        this.street,
+        this.postcode,
+        this.city,
+        this.phone)
+      );
+      this.employeeService.getEmployees().subscribe(_ => {
+        this.router.navigate(['employee']);
+      })
   }
-  cancelNewEmployyView(){
+  cancelNewEmployyView() {
     setTimeout(()=>{
       this.router.navigate(['employee']);
     }, 1000);
